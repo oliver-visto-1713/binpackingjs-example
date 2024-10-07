04aaebe88c62c0ee982763248e27bf13320064a2
yliaster <oliver.visto@kodakollectiv.com>
2024-10-07 14:33:40 +0800

    fix: rotation types

--- app/3d/_components/box-item.tsx
+++ app/3d/_components/box-item.tsx
@@ -1,56 +1,64 @@
 import { generateColor } from "@/constants/colors";
 import { DIVIDER } from "@/constants/divider";
-import { Vector3 } from "three";
+
 import * as THREE from "three";
 export default function BoxItem({ item, container, placedItems }: { item: any, container: any, placedItems: any[] }) {
-  const { _width, _height, _depth, _position, _rotationType } = item;
+  const { width, height, depth, position, rotationType } = item;
 
-  let w = _width / DIVIDER;
-  let h = _height / DIVIDER;
-  let d = _depth / DIVIDER;
+  let w = width / DIVIDER;
+  let h = height / DIVIDER;
+  let d = depth / DIVIDER;
   // Handle rotation based on rotationType
-  switch (_rotationType) {
+  // export const RotationTypeStrings = {
+  //   [RotationType_WHD]: 'RotationType_WHD (w,h,d)',
+  //   [RotationType_HWD]: 'RotationType_HWD (h,w,d)',
+  //   [RotationType_HDW]: 'RotationType_HDW (h,d,w)',
+  //   [RotationType_DHW]: 'RotationType_DHW (d,h,w)',
+  //   [RotationType_DWH]: 'RotationType_DWH (d,w,h)',
+  //   [RotationType_WDH]: 'RotationType_WDH (w,d,h)',
+  // };
+  switch (rotationType) {
     case 0: // No rotation
       // w, h, d remain the same
       break;
     case 1: // Rotate once (clockwise)
-      [w, h] = [h, w]; // Swap width and height
+      [w, h, d] = [h, w, d]; // Swap width and height
       break;
     case 2: // Rotate twice (clockwise)
-      [w, d] = [d, w]; // Swap width and depth
+      [w, h, d] = [h, d, w]; // Swap width and depth
       break;
     case 3: // Rotate thrice (clockwise)
-      [h, d] = [d, h]; // Swap height and depth
+      [w, h, d] = [d, h, w]; // Swap height and depth
       break;
     case 4: // Rotate counter-clockwise once
-      [h, w] = [w, h]; // Similar to clockwise once
+      [h, w] = [d, w, h]; // Similar to clockwise once
       break;
     case 5: // Rotate counter-clockwise twice
       // For a distinct behavior, let's swap width and depth again
-      [d, h] = [h, d]; // Swap height and depth
+      [w, h, d] = [w, d, h]; // Swap height and depth
       break;
     default:
       break;
   }
 
 
-  const cw = (container._width / DIVIDER) / 2;
-  const ch = (container._height / DIVIDER) / 2;
-  const cd = (container._depth / DIVIDER) / 2;
-  const x = _position[0] / DIVIDER
-  const y = _position[1] / DIVIDER
-  const z = _position[2] / DIVIDER
+  const cw = (container.width / DIVIDER) / 2;
+  const ch = (container.height / DIVIDER) / 2;
+  const cd = (container.depth / DIVIDER) / 2;
+  const x = position[0] / DIVIDER
+  const y = position[1] / DIVIDER
+  const z = position[2] / DIVIDER
   // Calculate the cumulative height of placed items for correct Y positioning
   // let cumulativeHeight = 0;
   // placedItems.forEach(placedItem => {
-  //   const placedHeight = placedItem._height / DIVIDER;
+  //   const placedHeight = placedItem.height / DIVIDER;
   //   cumulativeHeight += placedHeight; // Sum the heights of all placed items
   // });
 
   // Position calculations
-  // const posX = (_position[0] / DIVIDER + w / 2) - cw;
-  // const posY = (_position[1] / DIVIDER - (((_position[1] / DIVIDER) / 2) - h / 2) + h / 2) - ch;
-  // const posZ = (_position[2] / DIVIDER + d / 2) - cd;
+  // const posX = (position[0] / DIVIDER + w / 2) - cw;
+  // const posY = (position[1] / DIVIDER - (((position[1] / DIVIDER) / 2) - h / 2) + h / 2) - ch;
+  // const posZ = (position[2] / DIVIDER + d / 2) - cd;
   //- (x > 0 ? ((x / 2) - w / 2) : 0)
   //- (y > 0 ? ((y / 2) - h / 2) : 0)
   //- (z > 0 ? ((z / 2) - d / 2) : 0)
--- app/3d/_sections/canvas.tsx
+++ app/3d/_sections/canvas.tsx
@@ -4,34 +4,88 @@ import { Canvas } from "@react-three/fiber";
 import React, { useRef } from "react";
 import BoxContainer from "../_components/box-container";
 import BoxItem from "../_components/box-item";
-import { Bin, Item, Packer } from '@owens3364/3d-bin-packing';
+// import { Bin, Item, Packer } from '@owens3364/3d-bin-packing';
 
+import data from "@/app/3d/data/data.json"
 export default function BinCanvas() {
   const cameraControlRef = useRef(null);
+  const BinPacking3D = require("binpackingjs").BP3D;
+  const container = {
+    name: 'sample1',
+    x: 18,
+    y: 30,
+    z: 18
+  }
+  const items = [
+    {
+      name: "Sistema Food Storage Container Set, 28-pieces",
+      x: 13.75,
+      y: 8,
+      z: 9.5
+    },
+    {
+      "id": 98,
+      "x": 11,
+      "y": 9.75,
+      "z": 2.5,
+      "l": 11,
+      "w": 2.5,
+      "h": 9.75,
+      "name": "Dove Deep Moisture Body Wash, 3 x 680 mL",
+      "measurementType": "inches",
+      "color": "#FFD700"
+    },
+  ]
+  const { Item, Bin, Packer } = BinPacking3D;
+  const packer = new Packer();
+
+  const bin = new Bin("bin1", container.x, container.y, container.z)
+  const binContainer = { x: bin.width, y: bin.height, z: bin.depth };
+  for (const item of items) {
+    const i = new Item(item.name, item.x, item.y, item.z)
+    packer.addItem(i)
+  }
+  packer.addBin(bin);
+  packer.pack();
+  // let bin = new Bin("Bin A1", 50, 50, 50, 125000);
+  // let parcel1 = new Item("P1", 50, 25, 25, 200);
+  // let parcel2 = new Item("P2", 50, 25, 25, 200);
+  // let parcel3 = new Item("P3", 25, 25, 25, 150);
+  // let parcel4 = new Item("P4", 25, 25, 25, 150);
+  // let parcel5 = new Item("P4", 25, 12, 25, 50);
+  // let parcel6 = new Item("P4", 25, 12, 25, 50);
+  // let parcel7 = new Item("P4", 25, 12, 25, 50);
+  // let parcel8 = new Item("P4", 25, 12, 25, 50);
+
+  // let packer = new Packer();
+
+  // packer.addBin(bin);
+
+  // packer.addItem(parcel1);
+  // packer.addItem(parcel2);
+  // packer.addItem(parcel3);
+  // packer.addItem(parcel4);
+  // packer.addItem(parcel5);
+  // packer.addItem(parcel6);
+  // packer.addItem(parcel7);
+  // packer.addItem(parcel8);
+
+  // // pack items into bin1
+  // packer.pack();
   // const bp3d = require('../bp3d.js');
   // Define the bin with appropriate dimensions for packing
-  const bin1 = new Bin("Le petite box", 30, 25, 25);
+  // const containers = data.containers
+  // const packer = new Packer();
+  // const c = containers[0]
+  // const bin = new Bin(`bin-${c.id}`, c.x, c.y, c.z)
+  // packer.addBin(bin)
+  // for (const item of c.boxItems) {
+  //   const i = new Item(`item-${item.id}`, item.x, item.y, item.z)
+  //   packer.addItem(i)
+  // }
+  // packer.pack()
 
-  // Add multiple items of different sizes to fill up the bin
-  const itemsToPack = [
-    new Item("Item 1", 15, 12, 12),
-    new Item("Item 1", 15, 12, 12),
-    new Item("Item 1", 15, 12, 12),
-    new Item("Item 1", 12, 15, 12),
-  ];
-
-  // Initialize the packer and pack the items into the bin
-  const packer = new Packer();
-  packer.addBin(bin1);
-  itemsToPack.forEach(item => packer.addItem(item));  // Add all items
-  packer.pack() // Perform the packing operation
-
-  // Define the bin container dimensions for rendering
-  const binContainer = { x: bin1.width, y: bin1.height, z: bin1.depth };
-  // Get the packed items from the bin after packing
-  const packedItems = bin1.items;
-
-  console.log("bin", bin1);  // Log packed items for debugging
+  console.log(bin.items)
 
   return (
     <div className="flex w-full h-full">
@@ -45,13 +99,13 @@ export default function BinCanvas() {
 
           </React.Fragment>
           <React.Fragment>
-            {packedItems.map((item, index) => (
-              <BoxItem key={index} item={{ ...item }} container={bin1} placedItems={packedItems} />
+            {bin.items.map((item, index) => (
+              <BoxItem key={index} item={item} container={bin} placedItems={bin.items} />
             ))}
           </React.Fragment>
         </Canvas>
       </div>
-      <div className="bg-blue-500">Packed Items: {packedItems.length}</div>
+      <div className="bg-blue-500">Packed Items: {bin.items.length}</div>
     </div>
   );
 }
